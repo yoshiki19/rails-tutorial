fc4fb673b3ef1603a36fabb590763d5b24fe7aa2
yoshikatsu <you@example.com>
2022-08-30 06:35:22 +0000

    re

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,12 @@
 
 
 # ここから下をコメントアウトしてprocfileも同じようにコメントアウトするとpumaのサーバーに代わる(webickから）
-# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-# threads min_threads_count, max_threads_count
-# port        ENV.fetch("PORT") { 3000 }
-# environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
-# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-# preload_app!
-# plugin :tmp_restart
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
+plugin :tmp_restart
