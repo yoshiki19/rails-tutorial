0f12a4d16e95426df05a9c2b516e3eec408c3dd4
yoshikatsu <you@example.com>
2022-09-03 03:43:13 +0000

    fal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render new
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
   end
   
   def destroy
-    
+
   end
 end
